2f92b010e485af6094a8abd988fe775222f48cd0
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-02 00:18:27 +0530

    22_jan_assignment

---
+++ 22_jan.java
@@ -0,0 +1,20 @@
+// ans 1 = coumpter is a human made device , which makes our life easy by calculating tasks in the form of instructions, which is converted in binary digits (because computer is a dumb machine , it only understant two digits, ie 0 and 1) and executed by coumputer's cpu, computer contains majorly two components , the first one is hardware(keyboard, mouse, memory disk, physical memory etc) and second is software(os programs , word software, vs code sftware etc). 
+
+
+// ans 2 = RAM stands for random access memory that means we randomly access data which is present in RAM, because it is faster memory, it is also called as physical/main memory. RAM is a very important part of computer hardware because for execution of programs(process) we need to load it into main memory and then cpu execute those process's instruction.
+
+
+// ans 3 = data is stored in secondary memory as a paramanent storage and data stored as tempory manner in primary memory to get cpu's cycle.
+
+
+// ans 4 = that input device name is keyboard, and mouse(for digital keyboard)
+
+// ans 5 = output device examples are primary memory , secondary memory, monitor, webcam, arduino etc. 
+
+
+// ans 6 = mouse
+
+
+// ans 7 = binary language(0 and 1)
+
+// ans 8 = input devices are mouse, keyword , joystick, game controller, microphone, camera etc.
