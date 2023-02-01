5834efcd1ce61a25a9d546cde528d2a6850277e8
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-02 00:55:04 +0530

    23_jan_assignment

---
+++ 23_jan.java
@@ -0,0 +1,15 @@
+// ans 1 = git is used to control the different versions , so that software related projects can be easily managed and can have their backup.
+
+// ans 2 = version control means , controling the different versions which is created by their developer and maintaining their previous copies
+
+// ans 3 = github is used to store different files, it is also known as git server tool 
+
+// ans 4 = github, gitlab, bitbucket, gitblit etc
+
+// ans 5 = local version control system, centralized vcs, distributed vcs(mostly used)
+
+// ans 6 = advantages of git is its branching capabilities. Unlike centralized version control systems. This facilitates the feature branch workflow popular with many Git users.
+
+// ans 7 = git repository tracks and saves the history of all changes made to the files in a git project. It saves this data in a directory called . git , also known as the repository folder. git uses a version control system to track all changes made to the project and save them in the repository.
+
+// ans 8 = by using "git init" command
