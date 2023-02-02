b0495cc39c42fa699b12a8a89a9b4bf2d6093374
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-02 10:46:18 +0530

    26_jan_assignment

---
+++ jan_26.java
@@ -0,0 +1,11 @@
+// ans 1 = programming language is a language which is derievd from natural language and that lamnguage used to give instruction to cpu and perform tasks given by programmer 
+
+// ans 2 = because it helps to intract with machine and it is easy to use because it is made from natural language(eg. english etc) so that we can easily remember it's syntax and write programs easily , eg c, c++, java, c# etc.
+
+// ans 3 = java features are: Object-oriented, Secure, Portable, Robust, Multi-Threded, Garbage collection etc.
+
+// ans 4 = A Java object is a instance of a Java class. Each object has an identity, a behavior and a state. The state of an object is stored in fields (variables), while methods (functions) display the object's behavior. Objects are created at runtime from classes
+
+// ans 5 = class is a blueprint which defines some properties and behaviors, class is not allocated memory when it is defined,class is a template used to create objects and to define object data types and methods
+
+// ans 6 = the main method is the entry point of an executable program; it is where the program control starts and ends. Main is declared inside a class or struct. Main must be static and it need not be public
