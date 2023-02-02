c47de8e846009dfa115f7a37edb18289d5ce609d
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-02 20:33:27 +0530

    jan_30_assignment

---
+++ jan_30.java
@@ -0,0 +1,16 @@
+// ans 1 = class_name object_name = new class_name()
+
+
+// ans 2 = new keyword is used to allocate heap memory to objects, arrays etc and return the address of that memory block.
+
+
+// ans 3 = there are three types of variables in java , the first one is local variable, second is instance variable and third is static variable
+
+
+// ans 4 = variables declared within a method are local variables, the scope of the local variable is limited to the method it is declared inside and an instance variable is declared inside a class but outside of any method or block and it is accessible throughout the class.
+
+
+// ans 5 = for instance variable memory is allocated in heap area and for local variable memory is allocated in stack area
+
+
+// ans 6 = when we have to use same method which has same funtionality for different types and numbers of arguments then in this case we use method overloading, which mean multiple method with same name and fintionality is used with different numbers of arguments or different types of arguments.
