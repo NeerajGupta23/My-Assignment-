dfc0b3eea09ddca3b9310505ed472a8e8c2c0e04
NeerajGupta23 <230803neeraj@gmail.com>
2023-02-02 10:27:58 +0530

    24_jan_assignment

---
+++ 24_jan.java
@@ -0,0 +1,15 @@
+// ans 1 = from "git --version" command we can check the availability of git 
+
+// ans 2 = with the help of "git init" command
+
+// ans 3 = from " git config --global user.name "username" " for name and " git config --global user.email "email" " for email
+
+// ans 4 = by the command "git add ." or "git add filename_with_extension"
+
+// ans 5 =  with the help of "git rm --cached <file_name>" command
+
+// ans 6 = by the command " git commit -m "message" " 
+
+// ans 7 = To push the commit from the local repo to your remote repositories, run git push -u remote-name branch-name where remote-name is the nickname the local repo uses for the remote repositories and branch-name is the name of the branch to push to the repository.
+
+// ans 8 = git clone is how you get a local copy of an existing repository to work on. git pull is how you update that local copy with new commits from the remote repository.
